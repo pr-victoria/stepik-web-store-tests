4aff13c8f25c9de1ae1439dbc32db0649d32fb62
Victoria Prokopenko <prvict.work@gmail.com>
2019-07-13 13:31:13 +0300

    Change find_elements to visibility_of_elements

--- test_items.py
+++ test_items.py
@@ -2,11 +2,14 @@ import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_button_should_be_on_page(browser):
     browser.get(link)
-    add_btn_list = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    browser.wait = WebDriverWait(browser, 5)
+    add_btn_list = browser.wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".btn-add-to-basket")))
     print("Number of returned buttons: {}".format(len(add_btn_list)))
     assert len(add_btn_list) == 1, "Expected 1 returned button, instead of {}".format(len(add_btn_list))
